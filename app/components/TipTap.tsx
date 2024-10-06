a19dd7039a4cd89f948debc21aef986042f7b549
Jess <jessicalaurenwilson1@gmail.com>
2024-10-06 20:22:57 +0100

    create editor

--- app/components/TipTap.tsx
+++ app/components/TipTap.tsx
@@ -2,28 +2,63 @@
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import { useState } from 'react';
+// import { useState } from 'react';
 
-const Tiptap = () => {
-	const [editorContent, setEditorContent] = useState<string>('');
+interface TiptapProps {
+	onEditorContentSave: (html: string) => void;
+}
+
+const Tiptap = ({ onEditorContentSave }: TiptapProps) => {
+	// const [editorContent, setEditorContent] = useState();
+
+	// const content = editorContent;
 
 	const editor = useEditor({
 		extensions: [StarterKit],
 		editorProps: {
 			attributes: {
 				class:
-					'm-5 border-2 border-sky-500 rounded-md size-96 focus:border-0 focus:shadow-cyan-500/50 focus:shadow-lg p-1',
+					'bg-white m-5 border-2 border-sky-500 rounded-md size-96 focus:shadow-cyan-500/50 focus:shadow-lg p-1 outline-none',
 			},
 		},
-		content: '<p>Hello World! 🌎️</p>',
-		autofocus: true,
-		onUpdate: ({ editor }) => {
-			const text = editor.getText();
-			setEditorContent(text);
-		},
+		// content,
+		// autofocus: true,
+		// onUpdate: ({ editor }) => {
+		// 	const json = editor.getJSON();
+		// 	setEditorContent(json);
+		// },
 	});
 
-	return <EditorContent editor={editor} value={editorContent} />;
+	if (!editor) return null;
+
+	const handleEditorContent = () => {
+		const html = editor.getHTML();
+		// console.log(html);
+		onEditorContentSave(html);
+	};
+
+	return (
+		<>
+			<button
+				onClick={() => editor.chain().focus().toggleBold().run()}
+				disabled={!editor.can().chain().focus().toggleBold().run()}
+				className={editor.isActive('bold') ? 'is-active' : ''}
+			>
+				Bold
+			</button>
+			<div>
+				<EditorContent editor={editor} />
+			</div>
+			<div className='flex items-center justify-center'>
+				<button
+					className='w-64 py-2.5 px-5 text-center ml-10 my-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-sky-500'
+					onClick={handleEditorContent}
+				>
+					Save{' '}
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default Tiptap;
