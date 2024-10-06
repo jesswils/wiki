d942d72fa0567fb72b88228a9d101cd26419caf4
Jess <jessicalaurenwilson1@gmail.com>
2024-10-06 20:23:13 +0100

    refactor document

--- app/components/Document.tsx
+++ app/components/Document.tsx
@@ -1,27 +1,36 @@
 'use client';
 
-import React, { useState } from 'react';
-import Tiptap from './TipTap';
+import NewNote from './NewNote';
+// import Tiptap from './Tiptap';
+
+// import React, { useState } from 'react';
 
 const Document = () => {
-	const [content, setContent] = useState('');
+	// const [content, setContent] = useState('');
 
 	return (
 		<div>
 			<div className='flex space-x-12'>
-				<label className='text-base font-bold text-muted-foreground'>
+				{/* <label
+					className='text-base font-bold text-muted-foreground'
+					htmlFor='document title'
+				>
 					Title
 				</label>
 				<input
-					className='h-10 w-96 px-3 py-1.5 text-base border-2 border-sky-500 placeholder:text-sm placeholder:text-muted-foreground outline-none rounded focus:shadow-cyan-500/50 focus:shadow-md'
+					className='h-10 w-96 px-3 py-1.5 text-base border-2 border-sky-500 outline-none rounded focus:shadow-cyan-500/50 focus:shadow-md'
 					name='document title'
 					value={content}
 					onChange={(e) => setContent(e.target.value)}
-				/>
+				/> */}
 			</div>
 			<div className='flex'>
 				<p className='font-bold mt-5'>Content</p>
-				<Tiptap />
+				<NewNote />
+			</div>
+			{/* conditionally render */}
+			<div>
+				<h2 className='text-lg text-center pt-10'>You have no notes yet</h2>
 			</div>
 		</div>
 	);
