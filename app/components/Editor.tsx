fb22c725f4f61da863653fffae92f37f84ea66d8
Jess <jessicalaurenwilson1@gmail.com>
2024-10-07 18:49:02 +0100

    editor

--- app/components/Editor.tsx
+++ app/components/Editor.tsx
@@ -6,21 +6,25 @@ import Tiptap from './TipTap';
 
 const Editor = () => {
 	const [htmlContent, setHtmlContent] = useState<string>();
-	const [showNote, setShowNote] = useState<boolean>(false);
-	
 
 	const handleEditorContentSave = (html: string) => {
 		setHtmlContent(html);
-		setShowNote((prevState) => !prevState);
 	};
+	// set item into local storage or create a database to post the information to
+	// use effect to load the data on initial page load
+	// refactor the below to once we have made a successful request, display the notes
 	return (
 		<>
 			<Tiptap onEditorContentSave={handleEditorContentSave} />
 			<hr className='my-8 h-0.5 border-t-0 bg-neutral-100 opacity-100' />
 			<div className='flex'>
-			<div>
-				{showNote ? <ShowNote content={htmlContent} /> : <h2 className='text-lg text-center mt-0'>You have no notes yet</h2>} 
-			</div>
+				<div>
+					{htmlContent ? (
+						<ShowNote content={htmlContent} />
+					) : (
+						<h2 className='text-lg text-center mt-0'>You have no notes yet</h2>
+					)}
+				</div>
 			</div>
 		</>
 	);
