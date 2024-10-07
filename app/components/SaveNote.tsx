e24dbb87e66b95494eca0db229888a40655c0403
Jess <jess.wilson@brighthr.com>
2024-10-07 11:45:38 +0100

    save note

---
+++ app/components/SaveNote.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SaveNote = () => {
+  return (
+    <div>
+        <button
+					className='w-96 py-2.5 px-5 text-center ml-5  my-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border hover:bg-gray-100 hover:text-sky-500 focus:shadow-cyan-500/50'
+				>
+					Note
+				</button>
+    </div>
+  )
+}
+
+export default SaveNote
