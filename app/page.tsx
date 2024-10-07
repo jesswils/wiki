637bc1a504b7a4f95aba3efdc8d5e653d233e637
Jess <jess.wilson@brighthr.com>
2024-10-07 11:56:05 +0100

    amend headings from global styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 		<div className='flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
 			<main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
 				<h1 className='text-2xl'>〰️ Welcome to your wiki 〰️ </h1>
-				<h2 className='font-bold text-xl'>Add a new page</h2>
+				<h2 className='font-bold text-xl mb-0'>Add a new page</h2>
 				<Wiki />
 			</main>
 			<footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'></footer>
