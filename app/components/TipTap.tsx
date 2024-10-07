144f94b86f14d7e8ec5c2487d699f98e8654aff5
Jess <jess.wilson@brighthr.com>
2024-10-07 11:45:07 +0100

    tidyup

--- app/components/TipTap.tsx
+++ app/components/TipTap.tsx
@@ -2,38 +2,27 @@
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-// import { useState } from 'react';
 
 interface TiptapProps {
 	onEditorContentSave: (html: string) => void;
 }
 
 const Tiptap = ({ onEditorContentSave }: TiptapProps) => {
-	// const [editorContent, setEditorContent] = useState();
-
-	// const content = editorContent;
 
 	const editor = useEditor({
 		extensions: [StarterKit],
 		editorProps: {
 			attributes: {
 				class:
-					'bg-white m-5 border-2 border-sky-500 rounded-md size-96 focus:shadow-cyan-500/50 focus:shadow-lg p-1 outline-none',
+					'bg-white m-5 border border-sky-500 rounded-md size-96 focus:shadow-sky-200/50 focus:shadow-lg p-1 outline-none',
 			},
 		},
-		// content,
-		// autofocus: true,
-		// onUpdate: ({ editor }) => {
-		// 	const json = editor.getJSON();
-		// 	setEditorContent(json);
-		// },
 	});
 
 	if (!editor) return null;
 
 	const handleEditorContent = () => {
 		const html = editor.getHTML();
-		// console.log(html);
 		onEditorContentSave(html);
 	};
 
@@ -159,7 +148,7 @@ const Tiptap = ({ onEditorContentSave }: TiptapProps) => {
 				<EditorContent editor={editor} />
         </div>
 				<button
-					className='w-96 py-2.5 px-5 text-center ml-5  my-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-sky-500'
+					className='w-96 py-2.5 px-5 text-center ml-5  my-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border hover:bg-gray-100 hover:text-sky-500 focus:shadow-cyan-500/50'
 					onClick={handleEditorContent}
 				>
 					Save{' '}
--- app/components/Wiki.tsx
+++ app/components/Wiki.tsx
@@ -1,38 +1,9 @@
 'use client';
 
-import NewNote from './NewNote';
-// import Tiptap from './Tiptap';
+import Editor from './Editor';
 
-// import React, { useState } from 'react';
-
-const Document = () => {
-	// const [content, setContent] = useState('');
-
-	return (
-		<div>
-			<div className='flex space-x-12'>
-				{/* <label
-					className='text-base font-bold text-muted-foreground'
-					htmlFor='document title'
-				>
-					Title
-				</label>
-				<input
-					className='h-10 w-96 px-3 py-1.5 text-base border-2 border-sky-500 outline-none rounded focus:shadow-cyan-500/50 focus:shadow-md'
-					name='document title'
-					value={content}
-					onChange={(e) => setContent(e.target.value)}
-				/> */}
-			</div>
-			<div className='flex'>
-				<NewNote />
-			</div>
-			{/* conditionally render */}
-			<div>
-				<h2 className='text-lg text-center pt-10'>You have no notes yet</h2>
-			</div>
-		</div>
-	);
+const Wiki = () => {
+	return <Editor />;
 };
 
-export default Document;
+export default Wiki;
