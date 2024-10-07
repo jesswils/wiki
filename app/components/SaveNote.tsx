4aca95e34190e3b49c54f4f7a2f13f9105a0fec7
Jess <jess.wilson@brighthr.com>
2024-10-07 15:57:35 +0100

    save note wip

--- app/components/SaveNote.tsx
+++ app/components/SaveNote.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import Link from "next/link"
 
-const SaveNote = () => {
+interface SaveNoteProps {
+  content: string | JSX.Element | JSX.Element[]
+}
+
+const SaveNote = ({content}: SaveNoteProps) => { // I would add this content to context instead of prop drilling
   return (
     <div>
+      <Link href='/notes'>
         <button
-					className='w-96 py-2.5 px-5 text-center ml-5  my-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border hover:bg-gray-100 hover:text-sky-500 focus:shadow-cyan-500/50'
+					className='w-48 py-2.5 px-5 text-center ml-5  my-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border hover:bg-gray-100 hover:text-sky-500 focus:shadow-cyan-500/50'
 				>
 					Note
 				</button>
+        </Link>
+        {content}
     </div>
   )
 }
 
-// This is a placeholder component for when a user saves their note, it will be displayed as a button that they can click to view and edit their note in a seperate page.
+// This is a wip for when a user saves their note, it will be displayed as a button that they can click to view and edit their note in a seperate page.
 
 export default SaveNote
+
+// https://stackoverflow.com/questions/77384725/how-can-i-make-my-button-in-nextjs-go-to-another-page-i-created-in-the-same-app
