f2ec221ca188de6b2c1898e0171aa4002620a717
Jess <jessicalaurenwilson1@gmail.com>
2024-10-07 18:58:34 +0100

    save note

--- app/components/SaveNote.tsx
+++ app/components/SaveNote.tsx
@@ -1,26 +1,23 @@
-import Link from "next/link"
+import Link from 'next/link';
 
 interface SaveNoteProps {
-  content: string | JSX.Element | JSX.Element[]
+	content: string | JSX.Element | JSX.Element[];
 }
 
-const SaveNote = ({content}: SaveNoteProps) => { // I would add this content to context instead of prop drilling
-  return (
-    <div>
-      <Link href='/notes'>
-        <button
-					className='w-48 py-2.5 px-5 text-center ml-5  my-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border hover:bg-gray-100 hover:text-sky-500 focus:shadow-cyan-500/50'
-				>
+const SaveNote = ({ content }: SaveNoteProps) => {
+	// I would add this content to context instead of prop drilling
+	return (
+		<div>
+			<Link href='/notes'>
+				<button className='w-48 py-2.5 px-5 text-center ml-5  my-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border hover:bg-gray-100 hover:text-sky-500 focus:shadow-cyan-500/50'>
 					Note
 				</button>
-        </Link>
-        {content}
-    </div>
-  )
-}
+			</Link>
+			{content}
+		</div>
+	);
+};
 
-// This is a wip for when a user saves their note, it will be displayed as a button that they can click to view and edit their note in a seperate page.
+// This is a wip for when a user saves their note, they can click the button to view and edit their note in a seperate page.
 
-export default SaveNote
-
-// https://stackoverflow.com/questions/77384725/how-can-i-make-my-button-in-nextjs-go-to-another-page-i-created-in-the-same-app
+export default SaveNote;
