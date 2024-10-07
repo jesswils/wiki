e71620342141f45b2aa4b1ee6f3320a518d238d9
Jess <jess.wilson@brighthr.com>
2024-10-07 09:20:32 +0100

    parse content wip

--- app/components/ShowNote.tsx
+++ app/components/ShowNote.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import { parse } from 'node-html-parser';
 
 interface ShowNoteProps {
 	content: string | undefined;
 }
 
 const ShowNote = ({ content }: ShowNoteProps) => {
-	return <div>{content}</div>;
+	if(!content) return null;
+
+	const parsedContent = parse(content);
+	console.log(parsedContent)
+
+	return <div dangerouslySetInnerHTML={{ __html: parsedContent }} />
 };
 
 export default ShowNote;
